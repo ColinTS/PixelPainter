50c9f05a297dfadbb8336dab823a0eba0a6f8f1e
Charles1104 <charles.xavier.verleyen@gmail.com>
2017-03-25 12:14:41 -1000

    Buttons clear and erase working

--- js/ColorSwatch.js
+++ js/ColorSwatch.js
@@ -1,11 +1,10 @@
-
-
 var colorCache;
-
-// color swatch creation
-
 var pixelPainter = document.querySelector("#pixelPainter");
 var colorSwatch = document.createElement('div');
+
+
+// container containing color swatch and commands buttons
+
 var swatchCommandContainer = document.createElement('div');
 
 swatchCommandContainer.id = "swatchCommandContainer";
@@ -16,7 +15,7 @@ swatchCommandContainer.style.border = "10px solid orange";
 pixelPainter.appendChild(swatchCommandContainer);
 
 
-
+// Color swatch creation
 
 colorSwatch.id = "main_color_swatch";
 colorSwatch.style.height = "300px";
@@ -25,10 +24,12 @@ colorSwatch.style.border = "10px solid red";
 
 swatchCommandContainer.appendChild(colorSwatch);
 
+
 // Rows creations (one div per row)
 for (var i = 1 ; i <= 10; i++){
   var rows_colorSwatch = document.createElement('div');
   colorSwatch.appendChild(rows_colorSwatch);
+
 
 // Colums creation (each j will create a child to a i). Each one will have a color varying with i and j
     for (var j = 1 ; j <= 6; j++){
@@ -42,17 +43,12 @@ for (var i = 1 ; i <= 10; i++){
       rows_colorSwatch.appendChild(uniqueColors);
 
       uniqueColors.addEventListener('click', function() {
-        console.log('boom');
         colorCache = this.style.background;
-        console.log(colorCache);
       });
     }
 }
 
-
-'use strict';
-
-// Color Swatch creation
+// Main pixel panels creation
 
 var pixelPainter = document.querySelector("#pixelPainter");
 var palette = document.createElement('div');
@@ -69,9 +65,11 @@ for (var i = 1 ; i <= 60; i++){
   var rows = document.createElement('div');
   palette.appendChild(rows);
 
+
 // Colums creation (each j will create a child to a i)
     for (var j = 1 ; j <= 60; j++){
       var pixels = document.createElement('div');
+      pixels.className = "pixelsClass";
       pixels.id = "row" + i + "col" + j;
       pixels.style.height = "10px";
       pixels.style.width = "8px";
@@ -80,11 +78,48 @@ for (var i = 1 ; i <= 60; i++){
       rows.appendChild(pixels);
 
       pixels.addEventListener('click', function() {
-        console.log('pal');
         this.style.backgroundColor = colorCache;
+        console.log(this);
         console.log(colorCache);
       });
   }
 
 }
+
+// command buttons creation
+
+var clearButton = document.createElement('button');
+clearButton.id = "clear";
+clearButton.style.height = "50px";
+clearButton.style.width = "200px";
+clearButton.style.border = "10px solid black";
+clearButton.innerHTML = "CLEAR";
+
+swatchCommandContainer.appendChild(clearButton);
+
+var backgroundpanel = document.getElementsByClassName("pixelsClass");
+
+
+clear.addEventListener('click', function() {
+  for (var i = 0; i < backgroundpanel.length; i++) {
+    backgroundpanel[i].style.backgroundColor = "white";
+  }
+});
+
+
+var eraseButton = document.createElement('button');
+eraseButton.id = "erase";
+eraseButton.style.height = "50px";
+eraseButton.style.width = "200px";
+eraseButton.style.border = "10px solid black";
+eraseButton.innerHTML = "ERASE";
+
+swatchCommandContainer.appendChild(eraseButton);
+
+
+
+erase.addEventListener('click', function() {
+  colorCache = "rgb(255,255,255)";
+});
+
 
