122608e747e1e45d05d80f4f800286a7559620d7
Charles1104 <charles.xavier.verleyen@gmail.com>
2017-03-25 16:40:49 -1000

    First draft version

--- js/ColorSwatch.js
+++ js/ColorSwatch.js
@@ -34,17 +34,22 @@ for (var i = 1 ; i <= 10; i++){
 // Colums creation (each j will create a child to a i). Each one will have a color varying with i and j
     for (var j = 1 ; j <= 6; j++){
       var uniqueColors = document.createElement('div');
+      var randnumb1 = Math.floor(Math.random() * 255);
+      var randnumb2 = Math.floor(Math.random() * 255);
+      var randnumb3 = Math.floor(Math.random() * 255);
+
       uniqueColors.id = "rowc" + i + "colc" + j;
       uniqueColors.style.height = "30px";
       uniqueColors.style.width = "23px";
       uniqueColors.style.border = "1px solid black";
       uniqueColors.style.display = "table-cell";
-      uniqueColors.style.background = "rgb(" + (255) + "," + (255 - 25*i/j)  + "," + (250 - 40) +")";
+      uniqueColors.style.background = "rgb(" + (255 - randnumb1) + "," + (255 - randnumb2)  + "," + (255 - randnumb3) +")";
       rows_colorSwatch.appendChild(uniqueColors);
 
       uniqueColors.addEventListener('click', function() {
         colorCache = this.style.background;
       });
+
     }
 }
 
@@ -61,26 +66,39 @@ palette.style.border = "10px solid green";
 pixelPainter.appendChild(palette);
 
 // Rows creations (one div per row)
-for (var i = 1 ; i <= 60; i++){
+for (var i = 1 ; i <= 20; i++){
   var rows = document.createElement('div');
   palette.appendChild(rows);
 
 
 // Colums creation (each j will create a child to a i)
-    for (var j = 1 ; j <= 60; j++){
+    for (var j = 1 ; j <= 20; j++){
       var pixels = document.createElement('div');
       pixels.className = "pixelsClass";
       pixels.id = "row" + i + "col" + j;
-      pixels.style.height = "10px";
-      pixels.style.width = "8px";
+      pixels.style.height = "30px";
+      pixels.style.width = "28px";
       pixels.style.border = "1px solid black";
       pixels.style.display = "table-cell";
       rows.appendChild(pixels);
 
       pixels.addEventListener('click', function() {
         this.style.backgroundColor = colorCache;
-        console.log(this);
-        console.log(colorCache);
+      });
+
+      var mousedown = false;
+      document.onmousedown = function(){
+        mousedown = true;
+      };
+      document.onmouseup = function(){
+        mousedown = false;
+      };
+
+      pixels.addEventListener('mouseover', function(){
+        if(mousedown === true){
+          this.style.backgroundColor = colorCache;
+          console.log(mousedown);
+        }
       });
   }
 
@@ -91,8 +109,8 @@ for (var i = 1 ; i <= 60; i++){
 var clearButton = document.createElement('button');
 clearButton.id = "clear";
 clearButton.style.height = "50px";
-clearButton.style.width = "200px";
-clearButton.style.border = "10px solid black";
+clearButton.style.width = "160px";
+clearButton.style.border = "3px solid black";
 clearButton.innerHTML = "CLEAR";
 
 swatchCommandContainer.appendChild(clearButton);
@@ -110,8 +128,8 @@ clear.addEventListener('click', function() {
 var eraseButton = document.createElement('button');
 eraseButton.id = "erase";
 eraseButton.style.height = "50px";
-eraseButton.style.width = "200px";
-eraseButton.style.border = "10px solid black";
+eraseButton.style.width = "160px";
+eraseButton.style.border = "3px solid black";
 eraseButton.innerHTML = "ERASE";
 
 swatchCommandContainer.appendChild(eraseButton);
@@ -123,3 +141,16 @@ erase.addEventListener('click', function() {
 });
 
 
+// for (var i = 1 ; i <= 20; i++){
+//   for (var j = 1 ; j <= 20; j++){
+//     row i col j.addEventListener('drag', function() {
+
+//       if ((this.getBoundingClientRect().left <= event.clientX <= this.getBoundingClientRect().right) || (this.getBoundingClientRect().bottom <= event.clientY <= this.getBoundingClientRect().top))
+
+// console.log(event.clientX);
+// console.log(event.clientY);
+// console.log(this.getBoundingClientRect().left);
+// console.log(this.getBoundingClientRect().right);
+// console.log(this.getBoundingClientRect().top);
+// console.log(this.getBoundingClientRect().bottom);
+// });
